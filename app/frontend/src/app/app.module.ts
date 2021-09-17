c18076139c71c73c50175fadc2d4c6631f5c9c86
“Mikhail <mikhail_zavadski@epam.com>
2021-09-17 11:12:27 +0300

    frontend-initial

---
+++ app/frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DatasetListComponent } from './components/dataset-list/dataset-list.component';
+import { NewDatasetComponent } from './components/new-dataset/new-dataset.component';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DatasetListComponent,
+    NewDatasetComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/frontend/src/app/components/new-dataset/new-dataset.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-dataset',
+  templateUrl: './new-dataset.component.html',
+  styleUrls: ['./new-dataset.component.css']
+})
+export class NewDatasetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
