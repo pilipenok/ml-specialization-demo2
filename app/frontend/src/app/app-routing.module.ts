2fd5f2f7346b2e190358be2959219db8532a5151
“Mikhail <mikhail_zavadski@epam.com>
2021-09-17 11:41:45 +0300

    Added routes

--- app/frontend/src/app/app-routing.module.ts
+++ app/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DatasetListComponent } from './components/dataset-list/dataset-list.component';
+import { DatasetDetailsComponent } from './components/dataset-details/dataset-details.component';
+import { NewDatasetComponent } from './components/new-dataset/new-dataset.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+      {path: '', component: DatasetListComponent},
+      {path: 'dataset/:datasetId', component: DatasetDetailsComponent},
+      {path: 'new-dataset', component: NewDatasetComponent},
+      {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
