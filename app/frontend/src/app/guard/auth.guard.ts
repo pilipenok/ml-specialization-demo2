e62ef96339e93f4cfe5a68c998ed5357b790e16f
“Mikhail <mikhail_zavadski@epam.com>
2021-09-23 10:05:03 +0300

    Copyright

--- app/frontend/src/app/guard/auth.guard.ts
+++ app/frontend/src/app/guard/auth.guard.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright © 2021 EPAM Systems, Inc. All Rights Reserved. All information contained herein is,
+ * and remains the property of EPAM Systems, Inc. and/or its suppliers and is protected by
+ * international intellectual property law. Dissemination of this information or reproduction
+ * of this material is strictly forbidden, unless prior written permission is obtained
+ * from EPAM Systems, Inc
+ */
+
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
